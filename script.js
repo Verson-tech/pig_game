504462c5a118f4da8a7ab47879ba3a737a5154f3
vira.murray <vira.avramenko@gmail.com>
2022-05-06 17:18:48 +0200

    'play again' functinality

--- script.js
+++ script.js
@@ -14,9 +14,10 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //Starting conditions:
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const startingScore1 = score0El.textContent = 0;
+const startingScore2 = score1El.textContent = 0;
+const stratingDice = diceEl.classList.add('hidden');
+
 
 const scores = [0,0];
 let currentScore = 0;
@@ -72,3 +73,17 @@ btnHold.addEventListener('click', function(){
 }
 }
 })
+
+
+
+    btnNew.addEventListener('click', function(){
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    diceEl.classList.add('hidden');
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner');
+    playing
+})
+
+
